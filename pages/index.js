a01e6ba7bb72a3b228bd8658402d4d361be602da
Abhiraj Bhowmick <87644009+RainboeStrykr@users.noreply.github.com>
2024-11-03 22:49:47 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <article className="w-full px-10 py-10 mb-20 overflow-y-auto max-w-[620px] mx-auto">
         <div className="flex items-start justify-center w-full mb-10 overflow-hidden rounded-lg max-h-[500px] shadow-xl">
           <img
-            src="https://us-east-1.tixte.net/uploads/me.likes.cash/web-pfp.jpeg"
+            src="https://us-east-1.tixte.net/uploads/me.likes.cash/newpfp.jpg"
             alt="Abhiraj"
             title="A profile picture of Abhiraj"
           />
