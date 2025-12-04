9715f3085ec930ef239bc172e61e685a105cac83
rainboestrykr <abhirajbhowmick27@gmail.com>
2025-12-04 18:24:48 +0530

    edited next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,11 @@ module.exports = {
     twitter: "rainboestrykr",
   }
 }
+
+// next.config.js
+module.exports = {
+  webpack: (config) => {
+    config.resolve.fallback = { crypto: false };
+    return config;
+  },
+};
